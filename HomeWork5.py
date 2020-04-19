39c48584fe10119817dcf1b7265c544073376e33
Pr0venzano <Pr0venzano@mail.ru>
2020-04-19 17:07:02 +0300

    Task 4

--- HomeWork5.py
+++ HomeWork5.py
@@ -45,3 +45,24 @@ with open('text_3.txt', encoding='utf-8') as t_2:
             result += float(el[1])
             count += 1
         print(f'\nСредний доход сотрудников = {result/count}')
+
+
+# 4 Создать (не программно) текстовый файл со следующим содержимым:
+# One — 1
+# Two — 2
+# Three — 3
+# Four — 4
+# Необходимо написать программу, открывающую файл на чтение и считывающую построчно
+# данные. При этом английские числительные должны заменяться на русские. Новый блок строк
+# должен записываться в новый текстовый файл.
+
+from googletrans import Translator  # модуль гугл переводчика
+
+with open("text_4.txt", encoding='utf-8') as t_4:  # открыть существующий файл на чтение
+    content = t_4.readlines()  # прочитать содержимое файла
+    translator = Translator()  # создать переменную типа translator
+    for i in content:
+        result = translator.translate(i, src='en', dest='ru')  # перевод текста с eng на rus
+        with open("text_4ru.txt", "a", encoding='utf-8') as t_4ru:  # открыть (или создать) файл на дозапись
+            print(f'{result.text}\n', file=t_4ru)  # печать в файл 2 перевод текста файла 1
+
