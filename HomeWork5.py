c547cafcb34ff9e74eb4b289ff973eab475126ed
Pr0venzano <Pr0venzano@mail.ru>
2020-04-20 21:40:17 +0300

    Task 7

--- HomeWork5.py
+++ HomeWork5.py
@@ -99,3 +99,40 @@ with open('text_6.txt', encoding='utf-8') as t_6:
         numbers_value = [int(i) for i in digits_only.split()]  # генерация списка чисел из сроки, разделитель - пробел
         result.update({key: sum(numbers_value)})
     print(result)
+
+
+# 7 Создать вручную и заполнить несколькими строками текстовый файл, в котором каждая
+# строка должна содержать данные о фирме: название, форма собственности, выручка, издержки.
+# Пример строки файла: firm_1 ООО 10000 5000.
+# Необходимо построчно прочитать файл, вычислить прибыль каждой компании, а также среднюю прибыль.
+# Если фирма получила убытки, в расчет средней прибыли ее не включать. Далее реализовать список.
+# Он должен содержать словарь с фирмами и их прибылями, а также словарь со средней прибылью.
+# Если фирма получила убытки, также добавить ее в словарь (со значением убытков).
+# Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
+# Итоговый список сохранить в виде json-объекта в соответствующий файл.
+# Пример json-объекта: [{ "firm_1" : 5000 , "firm_2" : 3000 , "firm_3" : 1000 }, { "average_profit" : 2000 }]
+# Подсказка: использовать менеджер контекста.
+
+import json
+
+with open('text_7.txt', encoding='utf-8') as t_7:
+    while True:
+        content = t_7.read()  # построчное чтение файла через while
+        count = 0
+        result = {}
+        profit_sum = 0
+        average_profit = 0
+        if not content:
+            break
+        for i in content.split('\n'):
+            key = f'{i.split(" ")[0]} {i.split(" ")[1]}'
+            profit = int(i.split(" ")[2]) - int(i.split(" ")[3])  # выручка - издержки
+            result.update({key: profit})
+            if profit > 0:
+                count += 1
+                profit_sum += profit
+                average_profit = profit_sum/count
+            result_list = [result, {'average_profit': average_profit}]
+            with open("text_7.json", 'w', encoding="utf-8") as t_7j:
+                json.dump(result_list, t_7j, ensure_ascii=False, indent=4)
+        print(result_list)  # вывод результата программы на печать
